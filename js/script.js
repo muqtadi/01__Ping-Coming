d431f74436a176e15c81dcef8684ed56601143ad
muqtadi <abdullohmuqtadi@gmail.com>
2022-05-04 20:06:27 +0500

    Add mail js section

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const input = document.querySelector('.input');
+const addressBtn = document.querySelector('.address-btn');
+const invalidParagraph = document.querySelector('.invalid-email');
+const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+
+function myEmail(){
+  addressBtn.addEventListener('click', () =>{
+    if(input.value.match(mailFormat)){
+      console.log(true);
+    }
+    else{
+      invalidParagraph.getElementsByClassName.display = 'block';
+    }
+  })
+}
+myEmail();
